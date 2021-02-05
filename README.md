c1f813aed452b263060f443c3273924ec004fa27
Sam Hopkinson <s.hopkinson87@gmail.com>
2021-02-05 11:37:36 +0000

    05/02: Added pixel art, plus start

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 - Required:
     - Fix game logic. The luck score will be decided at random at the start of the game.
     - Events will need re-thinking. Not by severity? But by game progress. The further into
-    the game, the more deadly the enemies.
+    the game, the more deadly the enemies. Some kind of group
     - Each choice will have two outcomes, not one. Only one outcome will be returned with the 
     resource though, determined in the backend logic. The outcome will be determined by luck score.
     
@@ -11,3 +11,6 @@
 - Future:
     - Change luck bar for a message about luck.
     - Replace health bar with an image depicting health?
+    - Once this is done we can remove bootstrap and bootstrap vue.
+    - Fix tailwind css purge
+    - Make admin a part of the same vue instance / link to login auth route
--- resources/js/components/game/components/Start.vue
+++ resources/js/components/game/components/Start.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="flex flex-column justify-center align-center items-center h-7/8 sm:h-5/6">
-    <img class="w-12" src="images/asteroid.jpg">
-    <router-link to="/game" class="p-2 m-2 rounded-sm border-4 border-white">Start</router-link>
+        <div class="my-4 flex flex-column text-center container justify-center align-center items-center">
+            <img class="w-24" src="images/asteroid.jpg">
+            <p class="my-2">Civilisation has collapsed and mankind has turned on itself in a fight for survival.</p>
+            <p class="my-2">How long can you last before you succumb to your inevitable fate?</p>
+            <img class="w-12" src="images/skull.jpg">
+        </div>
+        <router-link to="/game" class="p-2 m-2 rounded-sm border-4 border-white hover:text-white hover:bg-green-500">Start</router-link>
     </div>
 </template>
 
 <script>
     export default {
-        methods: {
-
-        }
+        methods: {}
     }
 </script>
